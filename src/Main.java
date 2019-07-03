b898fc3a2701e3157a3ca928fb045495b1b3c008
Kirill <thegaldren@gmail.com>
2019-07-03 10:57:37 +0300

    read useful lines from file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String pathToFiles = "C:\\prog\\Java\\vk_posts_to_jekyll\\files";
         File[] files = new File(pathToFiles).listFiles();
+    }
+
+    private static ArrayList<String> readPost(File post){
+        ArrayList<String> postLines = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(post)){
+
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+
+                if(line.endsWith("---"))    // репосты не нужны
+                    return null;
+
+                if(!line.isBlank() && !line.endsWith("===")){
+                    postLines.add(line);
+                }
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return postLines;
     }
 }
