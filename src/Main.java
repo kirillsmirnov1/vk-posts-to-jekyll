8978d1987f1a893efbd36dc8b407fb8fa67bb41a
Kirill <thegaldren@gmail.com>
2019-07-03 10:25:39 +0300

    read list of files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        String pathToFiles = "C:\\prog\\Java\\vk_posts_to_jekyll\\files";
+        File[] files = new File(pathToFiles).listFiles();
     }
 }
